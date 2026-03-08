753e69b0d92186ce632af8fdc6d9638ea271a1d2
unknown <vivalaceres@proton.me>
2026-03-08 12:22:28 +0100

    test

--- obj/Debug/net10.0/SistemaBancario.AssemblyInfo.cs
+++ obj/Debug/net10.0/SistemaBancario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbc84d978f9dca6af005ae55c68950e847f3547")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccff768517dbb998f489c9c1e6587e9f8c1fca5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
